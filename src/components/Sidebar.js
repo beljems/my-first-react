ab7e7f5b1b5fb1c9f9b4b9ff21fe859b2f7d068d
Belle Jema <belle_jema@ligph.com>
2020-07-07 15:26:21 +0800

    Add categories on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 import './Sidebar.scss';
 import { ReactComponent as Logo } from './../logo.svg';
 
 const Sidebar = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(async () => {
+        async function getCategories() {
+            let res = await axios({
+                method: 'GET',
+                url: 'https://api.chucknorris.io/jokes/categories'
+            })
+
+            setCategories(res.data);
+        }
+
+        getCategories();
+    }, []);
+
   return (
     <aside className="sidebar">
         <a href="https://reactjs.org/" rel="noopener noreferrer" target="_blank">
@@ -19,6 +35,16 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/news">News</Link>
+                <ul>
+                    {categories.map((cat) => (
+                        <li>
+                            <Link to="">
+                                {cat}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+
                 <ul>
                     <li>
                         <Link to="/news/news-1">News 1</Link>
