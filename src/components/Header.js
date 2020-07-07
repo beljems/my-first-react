aae1f6846a904146892dea4b7de3bfc8e4de590b
Belle Jema <belle_jema@ligph.com>
2020-07-07 15:12:44 +0800

    Add api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+
+import axios from 'axios';
 
 import './Header.scss';
 
 const Header = () => {
-  return (
-    <header className="header">
-        <h1>
-            <Switch>
-                <Route path="/contact">
-                    Contact
-                </Route>
-                <Route path="/about">
-                    About
-                </Route>
-                <Route path="/news" exact>
-                    News
-                </Route>
-                <Route path="/news/:title"> 
-                    News
-                </Route>
-                <Route path="/" exact>
-                    Top
-                </Route>
-                <Route path="*">
-                    404
-                    {/*<Redirect path="/" />*/}
-                </Route>
-            </Switch>
-        </h1>
-    </header>
-  )
+    const [joke, setJoke] = useState('');
+
+    useEffect(async () => {
+        let res = await axios({
+            method: 'GET',
+            url: 'https://api.chucknorris.io/jokes/random'
+        })
+
+        setJoke(res.data.value);
+    }, [])
+
+
+    return (
+        <header className="header">
+            <h1>
+                <Switch>
+                    <Route path="/contact">
+                        Contact
+                    </Route>
+                    <Route path="/about">
+                        About
+                    </Route>
+                    <Route path="/news" exact>
+                        News
+                    </Route>
+                    <Route path="/news/:title">
+                        News
+                    </Route>
+                    <Route path="/" exact>
+                        Top
+                    </Route>
+                    <Route path="*">
+                        404
+                        {/*<Redirect path="/" />*/}
+                    </Route>
+                </Switch>
+                {joke}
+            </h1>
+        </header>
+    )
 }
 
 export default Header;
